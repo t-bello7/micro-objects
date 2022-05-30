7fad2550ff3a736e7dbbdb503ac034dbbe7eedc0
t-bello7 <bello4aus@gmail.com>
2022-05-30 17:29:44 +0100

    build: add linter checks

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
 const addBookForm = document.querySelector('.add-book-form');
-const bookContainer = document.querySelector('.books')
-let bookCollection = []
+const bookContainer = document.querySelector('.books');
+let bookCollection = [
+  {
+    title: 'haa',
+    author: 'be',
+  },
+];
 
 const renderElements = (arr, container) => {
-    arr.forEach((element) => {
-          container.innerHTML += `
+  arr.forEach((element) => {
+    container.innerHTML += `
           <div>
             <h2>${element.title}</h2>
-            <p>${element.description}</p>
+            <p>${element.author}</p>
             <button type="button"> Remove </button>
-          </div>`
-            
-        })
-}
+          </div>`;
+  });
+};
 
-
-if (JSON.parse(localStorage.getItem('book')) != null) {
-    bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
+if (JSON.parse(localStorage.getItem('bookCollection')) != null) {
+  bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
 }
 
 renderElements(bookCollection, bookContainer);
 
-
-
-addBookForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let book = {}
-    for (element of addBookForm.elements){
-        if (element.name === "title"){
-            element.addEventListener('change', (e)=>{
-                book.title = e.target.value;                
-            })
-        }
-        if (element.name === 'author'){
-            element.addEventListener('change', (e)=> {
-                book.author = e.target.value;
-            })
-        }
-    }
-
-    bookCollection.push(book)
-    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-    renderElements(bookCollection, bookContainer);
+const book = {};
+addBookForm.elements.forEach((element) => {
+  if (element.name === 'title') {
+    element.addEventListener('change', (e) => {
+      book.title = e.target.value;
+    });
+  }
+  if (element.name === 'author') {
+    element.addEventListener('change', (e) => {
+      book.author = e.target.value;
+    });
+  }
 });
 
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-
+  bookCollection.push(book);
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  addBookForm.submit();
+});
